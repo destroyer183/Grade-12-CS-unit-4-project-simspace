b30786355944a9b3902da5bbd5dcf3e716594c6a
destroyer183 <jzante99@gmail.com>
2025-05-21 14:24:58 -0400

    removed old stuff

--- src/core/main.ts
+++ src/core/main.ts
@@ -6,6 +6,7 @@ export enum FacilityType {
     MedicalCentre = "medical centre",
     Government = "government",
     PowerPlant = "power plant",
+    PDS = "planetary defense system",
     LuxuryHome = "luxury home",
     ComfortableHome = "comfortable home",
     AffordableHome = "affordable home",
@@ -14,55 +15,5 @@ export enum FacilityType {
     Environmental = "environmental",
     Store = "store",
     Restaurant = "restaurant",
-    Office = "office",
-    PDS = "planetary defense system"
+    Office = "office"
 };
-
-export class Player {
-
-    static instance: Player;
-
-    private readonly facilities: Facility[] = new Array(2500);
-
-    private facilityIndicies: number[] = [];
-
-    private money: number = 5000000000;
-
-    private population: number = 0;
-
-    private timer: number = 0;
-
-    constructor() {
-
-        if (Player.instance !== undefined) throw new Error("Error! can't create another instance of a singleton class.");
-        
-        Player.instance = this;
-    }
-
-    private gameTick() {
-
-        // check if disaster occurs
-
-        // loop over every facility
-
-        // update different variables by incramenting them by the facility attributes
-
-        // incrament power
-        // incrament population
-        // incrament revenue
-        // incrament pollution
-        // incrament maintenance costs
-
-
-    }
-
-    public createFacility(type: FacilityType) {
-
-
-        // switch case to make the object
-            // check if player has enough money
-            // check if surroundings satisfy building
-
-        // add facility to attributes if player has enough money
-    }
-}
