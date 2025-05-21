7ca6618c951df598f03a362273be568d65963fb7
KaranPj <karanpj06@gmail.com>
2025-05-21 19:02:08 +0000

    essential

--- src/facilities/EssentialService/PowerPlants.ts
+++ src/facilities/EssentialService/PowerPlants.ts
@@ -1,4 +1,6 @@
-class PowerPlants{
+import { EssentialServiceFacility } from "./EssentialServiceFacility";
+
+export class PowerPlants extends EssentialServiceFacility{
     protected buildCost : number = 500000000;
     protected maintenanceCost : number = 2000000;
     protected powerCost : number = 100;
